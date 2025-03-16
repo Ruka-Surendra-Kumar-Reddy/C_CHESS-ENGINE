b7fed73441af14d1d31a0b0abfc1950bd5900c03
Ruka Surendra kumar reddy <surendrakumarreddyr@gmail.com>
2025-03-16 20:46:34 +0530

    Initial commit

---
+++ ce.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+#include "defs.h"
+int main()
+{
+    printf("hello");
+    return 0;
+}
---
+++ defs.h
@@ -0,0 +1,10 @@
+#ifndef DEFS_H
+#define DEFINE_H
+
+typedef unsigned long long U64;
+
+#define NAME "CE 1.0"
+#define BRD_SQ_NUM 120
+
+#endif
+
